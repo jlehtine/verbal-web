b258ee17f043dd9cb256a4731ce6ed319ded1b8e
Elias Lehtinen <elias.a.lehtinen@gmail.com>
2023-06-28 15:39:11 +0300

    Dialog title

--- src/frontend/VerbalWebDialog.tsx
+++ src/frontend/VerbalWebDialog.tsx
@@ -1,12 +1,50 @@
-import { Dialog, DialogProps, TextField } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { Dialog, DialogProps, DialogTitle, DialogTitleProps, IconButton, TextField } from '@mui/material';
 import React from 'react';
 
-interface VerbalWebDialogProps extends DialogProps {}
+interface VerbalWebDialogProps extends DialogProps {
+    onClose: () => void;
+}
+
+interface VerbalWebDialogTitleProps extends DialogTitleProps {
+    onClose: () => void;
+}
 
 export default function VerbalWebDialog(props: VerbalWebDialogProps) {
+    const handleClose = props.onClose;
+    const open = props.open;
+
     return (
         <Dialog {...props}>
+            <VerbalWebDialogTitle onClose={handleClose}>Verbal Web AI assistant</VerbalWebDialogTitle>
             <TextField></TextField>
         </Dialog>
+    );
+}
+
+function VerbalWebDialogTitle(props: VerbalWebDialogTitleProps) {
+    const { children, onClose, ...other } = props;
+
+    return (
+        <DialogTitle>
+            {children}
+            {onClose ? (
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    sx={
+                        {
+                            /*
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500]
+                        */
+                        }
+                    }>
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </DialogTitle>
     );
 }
