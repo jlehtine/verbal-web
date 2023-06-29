6059a78275b16c4c638fa268239454aa148d6d3b
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2023-06-29 14:08:11 +0300

    Separate Typescript configuration for the backend.

--- webpack.config.js
+++ webpack.config.js
@@ -13,41 +13,67 @@ const babelOptions = {
     ],
 };
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        frontend: './src/frontend/frontend.tsx',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'verbal-web-[name].js',
-    },
-    module: {
-        rules: [
-            // Typescript code
-            {
-                test: /src[/\\]frontend[/\\].*\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: babelOptions,
-                    },
-                    {
-                        loader: 'ts-loader',
-                    },
-                ],
-            },
+module.exports = [
+    {
+        mode: 'production',
+        entry: './src/frontend/frontend.tsx',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'verbal-web-frontend.js',
+        },
+        module: {
+            rules: [
+                // Typescript code
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: babelOptions,
+                        },
+                        {
+                            loader: 'ts-loader',
+                        },
+                    ],
+                },
 
-            // Source map loader for output files
-            {
-                test: /dist[/\\]verbal-web-frontend\.js$/,
-                exclude: /node_modules/,
-                use: 'source-map-loader',
-            },
-        ],
+                // Source map loader for output files
+                {
+                    test: /dist[/\\]verbal-web-frontend\.js$/,
+                    exclude: /node_modules/,
+                    use: 'source-map-loader',
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '...'],
+        },
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '...'],
+    {
+        mode: 'production',
+        target: 'node',
+        entry: './src/backend/backend.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'verbal-web-backend.js',
+        },
+        module: {
+            rules: [
+                // Typescript code
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '...'],
+        },
     },
-};
+];
