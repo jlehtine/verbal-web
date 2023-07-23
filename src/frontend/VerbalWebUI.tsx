c46aa226f76fa18ce11d1a8515994cf55ca6cb5e
Elias Lehtinen <elias.a.lehtinen@gmail.com>
2023-07-23 13:10:47 +0300

    Read page content html

--- src/frontend/VerbalWebUI.tsx
+++ src/frontend/VerbalWebUI.tsx
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 
 export interface VerbalWebConfiguration {
     backendURL: string;
+    pageContentSelector: string;
 }
 
 interface VerbalWebUIProps {
@@ -13,10 +14,27 @@ interface VerbalWebUIProps {
 }
 
 export default function VerbalWebUI({ conf }: VerbalWebUIProps) {
+    // TODO: change default value
+    const defaultPageContentSelector = "h1, h2, p";
+    // Set default value if no pageContentSelector string given as conf parameter
+    const pageContentSelector = conf.pageContentSelector ?? defaultPageContentSelector;
+
     const [open, setOpen] = useState(false);
 
     function handleQuery(query: Message[]): Promise<string> {
         const data: BackendRequest = { query: query };
+        // Read page content
+        // TODO: only read text, not all html
+        const nodeList = document.querySelectorAll(pageContentSelector);
+        // Page content to be added to the query
+        let pageContent = "";
+        nodeList.forEach((node) => {
+            pageContent = pageContent + node.outerHTML;
+        });
+        console.log("nodeList(0): " + nodeList.item(0));
+        console.log("nodeList length: " + nodeList.length);
+        console.log("pageContentSelector: " + pageContentSelector);
+        console.log("pageContent: " + pageContent);
 
         return fetch(conf.backendURL + "/query", {
             method: "POST",
@@ -43,7 +61,7 @@ export default function VerbalWebUI({ conf }: VerbalWebUIProps) {
     }
 
     return (
-        <Box>
+        <Box className="verbal-web">
             <Tooltip title="Verbal Web AI assistant">
                 <IconButton
                     color="primary"
