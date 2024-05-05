e0aa37f8a7ed852831e9b022bbc664b1ea499204
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2024-05-05 21:50:10 +0300

    Fix moderation cache handling and make expiration limits configurable via environment.

--- src/backend/moderation.ts
+++ src/backend/moderation.ts
@@ -10,17 +10,20 @@ export class ModerationRejected extends Error {
 
 interface CachedModeration {
     created: number;
-    accepted: boolean;
+    flagged: boolean;
 }
 
 type ModerationCache = Map<string, CachedModeration>;
 
 const moderationCache: ModerationCache = new Map();
-let moderationCacheSize = 0;
 let moderationCacheCleaned = 0;
 
-const MODERATION_CACHE_EXPIRE_MILLIS = 60 * 60 * 1000;
-const MODERATION_CACHE_EXPIRE_ENTRIES = 1000;
+const MODERATION_CACHE_EXPIRE_MILLIS = process.env.VW_MODERATION_CACHE_EXPIRE_SECONDS
+    ? parseInt(process.env.VW_MODERATION_CACHE_EXPIRE_SECONDS) * 1000
+    : 60 * 60 * 1000;
+const MODERATION_CACHE_EXPIRE_ENTRIES = process.env.VW_MODERATION_CACHE_EXPIRE_ENTRIES
+    ? parseInt(process.env.VW_MODERATION_CACHE_EXPIRE_ENTRIES)
+    : 1000;
 const MODERATION_CACHE_CLEAN_MILLIS = 60 * 1000;
 const MODERATION_CACHE_CLEAN_ENTRIES = 1.1 * MODERATION_CACHE_EXPIRE_ENTRIES;
 
@@ -33,7 +36,7 @@ export function checkModeration(msg: string, openai: OpenAI): Promise<void> {
     const cached = moderationCache.get(msg);
     if (cached) {
         return new Promise((resolve, reject) => {
-            if (cached.accepted) {
+            if (!cached.flagged) {
                 resolve();
             } else {
                 reject(new ModerationRejected(msg));
@@ -41,17 +44,20 @@ export function checkModeration(msg: string, openai: OpenAI): Promise<void> {
         });
     } else {
         return openai.moderations.create({ input: msg }).then((response) => {
-            response.results.forEach((r) => {
-                if (r.flagged) {
-                    throw new ModerationRejected(msg);
-                }
-            });
+            const flagged = response.results
+                .map((r) => r.flagged)
+                .reduce((accumulator, currentValue) => accumulator || currentValue, false);
+            moderationCache.set(msg, { created: Date.now(), flagged: flagged });
+            if (flagged) {
+                throw new ModerationRejected(msg);
+            }
         });
     }
 }
 
 function cleanModerationCache() {
     const now = Date.now();
+    const moderationCacheSize = moderationCache.size;
     let entryExpireLimit = Number.MAX_VALUE;
     if (moderationCacheSize > MODERATION_CACHE_CLEAN_ENTRIES) {
         const createdSorted = Array.from(moderationCache.values())
@@ -65,7 +71,6 @@ function cleanModerationCache() {
         for (const [msg, entry] of moderationCache.entries()) {
             if (entry.created <= expireLimit) {
                 moderationCache.delete(msg);
-                moderationCacheSize--;
             }
         }
         moderationCacheCleaned = now;
