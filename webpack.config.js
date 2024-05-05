f326e3944bf62b7c8b7a1cd823de59ad9b33f2bc
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2024-05-05 14:46:58 +0300

    Update to core-js 3.37.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const babelOptions = {
             "@babel/preset-env",
             {
                 useBuiltIns: "usage",
-                corejs: "3.31",
+                corejs: "3.37",
             },
         ],
         "@babel/preset-react",
