58328ad7e9f03385c27a56794da02c24bf54cc8b
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2024-05-09 22:42:44 +0300

    Use Express.js as the web framework for backend.

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -1,13 +1,14 @@
 import { isBackendRequest } from "../shared/api";
 import { describeError } from "../shared/error";
-import { HttpMethod } from "./httpmethods";
 import { logInterfaceData } from "./log";
 import { query } from "./query";
-import { readFile } from "fs/promises";
-import { IncomingMessage, ServerResponse, createServer } from "http";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import { OpenAI } from "openai";
 import path from "path";
+
+const FRONTEND_JS = "verbal-web-frontend.js";
 
 // Initialize OpenAI API
 console.log("Initializing OpenAI API");
@@ -19,123 +20,78 @@ const openai = new OpenAI({
     apiKey: apiKey,
 });
 
-const server = createServer({}, (req, resp) => {
-    const allowOrigin = process.env.VW_ALLOW_ORIGIN ?? "*"; // "*" is default value
-    // const allowOrigin = "https://google.com"; // for testing purposes
+// Use Express.js
+const backend = express();
 
-    try {
-        logRequest(req);
-        if (req.url === "/verbal-web-frontend.js") {
-            setCorsHeaders(resp, allowOrigin, ["GET"]);
-            if (req.method === "OPTIONS") {
-                resp.end();
-            } else if (req.method === "GET") {
-                resp.statusCode = StatusCodes.OK;
-                resp.setHeader("content-type", "text/javascript");
-                readFile("verbal-web-frontend.js")
-                    .then((data) => {
-                        resp.setHeader("content-length", data.byteLength);
-                        resp.write(data);
-                        resp.end();
-                    })
-                    .catch(catchUnexpectedFunc(resp));
-            } else {
-                resp.statusCode = StatusCodes.METHOD_NOT_ALLOWED;
-                resp.end();
-            }
-        } else if (req.url?.match(/^\/test\/\w+\.html$/)) {
-            const file = path.join(...req.url.split("/").slice(1));
-            setCorsHeaders(resp, allowOrigin, ["GET"]);
-            if (req.method === "OPTIONS") {
-                resp.end();
-            } else if (req.method === "GET") {
-                resp.statusCode = StatusCodes.OK;
-                resp.setHeader("content-type", "text/html");
-                readFile(file)
-                    .then((data) => {
-                        resp.setHeader("content-length", data.byteLength);
-                        resp.write(data);
-                        resp.end();
-                    })
-                    .catch(catchUnexpectedFunc(resp));
-            } else {
-                resp.statusCode = StatusCodes.METHOD_NOT_ALLOWED;
-                resp.end();
-            }
-        } else if (req.url === "/query") {
-            setCorsHeaders(resp, allowOrigin, ["POST"]);
-            if (req.method === "OPTIONS") {
-                resp.end();
-            } else if (req.method === "POST" && req.headers["content-type"] === "application/json") {
-                req.setEncoding("utf8");
-                // Read and process data
-                let data = "";
-                req.on("data", (chunk) => {
-                    if (typeof chunk === "string") {
-                        data += chunk;
-                    } else {
-                        throw new Error(`Received a chunk of unexpected type ${typeof chunk}`);
-                    }
-                });
-                console.log(data);
-                req.on("end", () => {
-                    const breq: unknown = JSON.parse(data);
-                    logInterfaceData("Received frontend request", breq);
-                    if (isBackendRequest(breq)) {
-                        query(breq, openai)
-                            .then((bresp) => {
-                                logInterfaceData("Returning frontend response", bresp);
-                                resp.statusCode = StatusCodes.OK;
-                                resp.setHeader("content-type", "application/json");
-                                resp.write(JSON.stringify(bresp));
-                                resp.end();
-                            })
-                            .catch(catchUnexpectedFunc(resp));
-                    } else {
-                        resp.statusCode = StatusCodes.BAD_REQUEST;
-                        resp.setHeader("content-type", "text/plain");
-                        resp.write(ReasonPhrases.BAD_REQUEST);
-                        resp.end();
-                    }
-                });
-            } else {
-                resp.statusCode = StatusCodes.METHOD_NOT_ALLOWED;
-                resp.end();
-            }
-        } else {
-            serverError("Not found: " + (req.url ?? ""), StatusCodes.NOT_FOUND, resp);
-        }
-    } catch (err) {
-        catchUnexpectedFunc(resp)(err);
-    }
+// Log all requests
+backend.use((req, res, next) => {
+    logRequest(req);
+    next();
 });
 
-function setCorsHeaders(resp: ServerResponse, allowOrigin: string, methods: HttpMethod[]) {
-    resp.setHeader("Access-Control-Allow-Origin", allowOrigin);
-    resp.setHeader("Access-Control-Allow-Methods", methods.join(", "));
-    resp.setHeader("Access-Control-Allow-Headers", "*");
-}
+// Set CORS headers for all responses
+backend.use(cors({ origin: process.env.VW_ALLOW_ORIGIN }));
 
-// msg=error message, code=HTML status code
-function serverError(msg: string, code: number, resp: ServerResponse) {
-    console.error("ERROR: " + msg);
-    resp.statusCode = code;
-    resp.end();
-}
+// Serve static test files, if available
+backend.use("/test", express.static("test"));
 
-function catchUnexpectedFunc(resp: ServerResponse) {
+// Serve frontend Javascript
+backend.get("/" + FRONTEND_JS, (req, res) => {
+    res.sendFile(path.resolve(FRONTEND_JS));
+});
+
+// Answer queries
+backend.post("/query", (req, res) => {
+    if (!hasJsonContent(req)) {
+        res.sendStatus(StatusCodes.BAD_REQUEST);
+        return;
+    }
+    req.setEncoding("utf8");
+    let data = "";
+    req.on("data", (chunk) => {
+        if (typeof chunk === "string") {
+            data += chunk;
+        } else {
+            throw new Error(`Received a chunk of unexpected type ${typeof chunk}`);
+        }
+    });
+    console.log(data);
+    req.on("end", () => {
+        const breq: unknown = JSON.parse(data);
+        logInterfaceData("Received frontend request", breq);
+        if (isBackendRequest(breq)) {
+            query(breq, openai)
+                .then((bresp) => {
+                    logInterfaceData("Returning frontend response", bresp);
+                    res.json(bresp);
+                })
+                .catch(catchUnexpectedFunc(res));
+        } else {
+            res.sendStatus(StatusCodes.BAD_REQUEST);
+        }
+    });
+});
+
+function catchUnexpectedFunc(resp: Response) {
     return (err: unknown) => {
         console.error(describeError(err, true, "ERROR"));
-        resp.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
-        resp.end();
+        resp.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
     };
 }
 
-function logRequest(req: IncomingMessage) {
+function logRequest(req: Request) {
     if (req.method && req.url) {
         console.log(`Processing request: ${req.method} ${req.url}`);
     }
 }
 
-console.log("Start listening for requests");
-server.listen(8080);
+function hasJsonContent(req: Request): boolean {
+    const contentType = req.headers["content-type"];
+    return contentType ? contentType.split(";")[0].trim().toLowerCase() === "application/json" : false;
+}
+
+// Start listening for requests
+const port = process.env.VW_HTTP_PORT ? parseInt(process.env.VW_HTTP_PORT) : 8080;
+backend.listen(port, () => {
+    console.log(`Started listening on port ${port.toString()}`);
+});
