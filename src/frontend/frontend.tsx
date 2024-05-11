9d0871b2cfd584ac6a3c3870991b69df5d01c331
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2024-05-11 23:57:27 +0300

    Fix linting errors.

--- src/frontend/frontend.tsx
+++ src/frontend/frontend.tsx
@@ -1,3 +1,4 @@
+import { describeError } from "../shared/error";
 import { VerbalWebConfiguration } from "./VerbalWebUI";
 
 declare global {
@@ -11,10 +12,14 @@ function initVerbalWeb(elementId: string, conf: VerbalWebConfiguration) {
             import(/* webpackPrefetch: true */ "react"),
             import(/* webpackPrefetch: true */ "react-dom/client"),
             import(/* webpackPrefetch: true */ "./VerbalWebUI"),
-        ]).then(([{ default: React }, { createRoot: createRoot }, { default: VerbalWebUI }]) => {
-            const root = createRoot(elem);
-            root.render(<VerbalWebUI conf={conf} />);
-        });
+        ])
+            .then(([{ default: React }, { createRoot: createRoot }, { default: VerbalWebUI }]) => {
+                const root = createRoot(elem);
+                root.render(<VerbalWebUI conf={conf} />);
+            })
+            .catch((err: unknown) => {
+                console.error(describeError(err, false, "Failed to load modules"));
+            });
     } else {
         console.error("Verbal Web container element not fount: #%s", elementId);
     }
