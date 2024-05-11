f89c44fd425c6286fcb961843ac08a838fd9602d
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2024-05-11 09:20:21 +0300

    Backend command line argument handling. Support configurable static content directories. Support changing directory ons start.

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -11,6 +11,73 @@ import path from "path";
 
 const FRONTEND_JS = "verbal-web-frontend.js";
 
+interface StaticContent {
+    path?: string;
+    dir: string;
+}
+
+// Usage
+function usage() {
+    const basename = process.argv[1].replace(/^.*[/\\]/, "");
+    console.log(`usage: node ${basename} [option]...
+
+options:
+  -h, --help    print this help text
+  -p, --port PORT
+                listen to the specified port (default is 8080)
+  --chdir DIR   switch to DIR on startup
+  --static [/PATH:]DIR
+                serve static content from DIR either as /PATH or at root,
+                repeat the option to serve content from multiple directories
+`);
+}
+
+// Read arguments
+let port = process.env.VW_PORT ? parseInt(process.env.VW_PORT) : 8080;
+let chdir = process.env.VW_CHDIR;
+const staticContent: StaticContent[] = [];
+for (let i = 2; i < process.argv.length; i++) {
+    const a = process.argv[i];
+    if (a === "-h" || a === "--help") {
+        usage();
+        process.exit(0);
+    } else if (a === "--chdir") {
+        chdir = safeNextArg(process.argv, ++i);
+    } else if (a === "--static") {
+        staticContent.push(parseStatic(safeNextArg(process.argv, ++i)));
+    } else {
+        console.error(`Unexpected command line argument: ${a}`);
+        process.exit(1);
+    }
+}
+if (process.env.VW_STATIC) {
+    staticContent.push(...process.env.VW_STATIC.split(";").map(parseStatic));
+}
+
+function safeNextArg(argv: string[], i: number) {
+    if (i < argv.length) {
+        return argv[i];
+    } else {
+        console.error("Unexpected end of command line arguments");
+        process.exit(1);
+    }
+}
+
+function parseStatic(arg: string): StaticContent {
+    const ic = arg.indexOf(":");
+    if (arg.startsWith("/") && ic > 0) {
+        return { path: arg.substring(0, ic), dir: arg.substring(ic + 1) };
+    } else {
+        return { dir: arg };
+    }
+}
+
+// Switch to specified directory
+if (chdir !== undefined) {
+    console.log(`Changing directory to ${chdir}`);
+    process.chdir(chdir);
+}
+
 // Initialize OpenAI API
 console.log("Initializing OpenAI API");
 const apiKey = process.env.OPENAI_API_KEY;
@@ -33,9 +100,6 @@ backend.use((req, res, next) => {
 // Set CORS headers for all responses
 backend.use(cors({ origin: process.env.VW_ALLOW_ORIGIN }));
 
-// Serve static test files, if available
-backend.use("/test", express.static("test"));
-
 // Serve frontend Javascript
 backend.get("/" + FRONTEND_JS, (req, res) => {
     res.sendFile(path.resolve(FRONTEND_JS));
@@ -43,7 +107,7 @@ backend.get("/" + FRONTEND_JS, (req, res) => {
 
 // Answer queries
 backend.post("/query", bodyParser.json(), (req, res) => {
-    const breq = req.body;
+    const breq: unknown = req.body;
     logInterfaceData("Received frontend request", breq);
     if (isBackendRequest(breq)) {
         query(breq, openai)
@@ -56,6 +120,13 @@ backend.post("/query", bodyParser.json(), (req, res) => {
         res.sendStatus(StatusCodes.BAD_REQUEST);
     }
 });
+
+// Serve static files, if so instructed
+for (const sc of staticContent) {
+    const path = sc.path ?? "/";
+    console.log(`Serving static content from ${sc.dir} at path ${path}`);
+    backend.use(path, express.static(sc.dir));
+}
 
 function catchUnexpectedFunc(resp: Response) {
     return (err: unknown) => {
@@ -71,7 +142,6 @@ function logRequest(req: Request) {
 }
 
 // Start listening for requests
-const port = process.env.VW_HTTP_PORT ? parseInt(process.env.VW_HTTP_PORT) : 8080;
 backend.listen(port, () => {
     console.log(`Started listening on port ${port.toString()}`);
 });
