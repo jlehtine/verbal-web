36dfaac4eb7a422f708841c524bb5125a313a0db
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2024-05-27 23:07:48 +0300

    Stylesheet: Use more eye friendly colors for background and text.

--- src/frontend/defaultTheme.tsx
+++ src/frontend/defaultTheme.tsx
@@ -1,13 +1,33 @@
-import { createTheme, useMediaQuery } from "@mui/material";
+import { PaletteMode, createTheme, useMediaQuery } from "@mui/material";
 import React from "react";
 
 export function defaultTheme() {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+    const mode: PaletteMode = prefersDarkMode ? "dark" : "light";
     return React.useMemo(
         () =>
             createTheme({
                 palette: {
-                    mode: prefersDarkMode ? "dark" : "light",
+                    mode: mode,
+                    ...(mode === "light"
+                        ? {
+                              text: {
+                                  primary: "#333333",
+                              },
+                              background: {
+                                  default: "#f5f5f5",
+                                  paper: "#f5f5f5",
+                              },
+                          }
+                        : {
+                              text: {
+                                  primary: "#cccccc",
+                              },
+                              background: {
+                                  default: "#121212",
+                                  paper: "#121212",
+                              },
+                          }),
                 },
             }),
         [prefersDarkMode],
